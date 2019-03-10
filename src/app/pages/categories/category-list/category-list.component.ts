99d3bf700754142dc00c901586b334da2692bd27
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-10 00:44:48 -0300

    criado metodo para remover categoria

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
 import { error } from 'util';
+import { element } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-category-list',
@@ -24,6 +25,17 @@ export class CategoryListComponent implements OnInit {
       categories => this.categories = categories,
       error => alert('Erro ao listar')
     )
+  }
+
+  deleteCategory(category: Category){
+    const confirmDelete = confirm('Deseja remover o registro?');
+
+    if (confirmDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert('Erro ao remover registro')
+      )
+    }
   }
 
 }
