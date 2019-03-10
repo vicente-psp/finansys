f6c77d029502ac48d62b28ae1c5678172427377f
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-10 20:24:28 -0300

    atualizando componente category-form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -39,6 +39,17 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     this.setPageTitle();
   }
 
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction === 'new'){
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
+  }
+
+
   private setCurrentAction(){
     if (this.route.snapshot.url[0].path === 'new'){
       this.currentAction = 'new';
@@ -65,17 +76,57 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
           this.category = category;
           this.categoryForm.patchValue(this.category);
         },
-        (error) => alert('Ocorreu um erro no servdor')
+        (error) => alert('Ocorreu um erro no servidor')
       )
     }
   }
 
-  private setPageTitle(){
+  private setPageTitle() {
     if (this.currentAction === 'new'){
       this.pageTitle = 'Cadastro de nova categoria';
     } else {
       const categoryName = this.category.name || '';
-      this.pageTitle = 'Editando a categoria ' + categoryName;
+      this.pageTitle = 'Editando a categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  private updateCategory() {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+
+
+  private actionsForSuccess(category: Category) {
+    toastr.success('Solicitação processada com sucesso');
+
+    this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error) {
+    toastr.error('Ocorreu um erro ao processar solicitação');
+
+    this.submittingForm = false;
+
+    if (error.status = 422) {
+      this.serverErrorMessages = JSON.parse(error._body);
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor, tente mais tarde'];
     }
   }
 
