e28857cfb932284497ab024ce258b76fc47cc9b9
unknown <vicente.pspenha@gmail.com>
2019-03-11 20:35:50 -0300

    criando serviço para entry

---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
+
+import { Entry } from './entry.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  private END_POINT: string = 'api/entries';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAll(): Observable<Entry[]> {
+    return this.httpClient.get(this.END_POINT).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntries)
+    )
+  }
+
+  getById(id: number): Observable<Entry> {
+    const url = `${this.END_POINT}/${id}`;
+
+    return this.httpClient.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    )
+  }
+
+  create(entry: Entry): Observable<Entry> {
+    return this.httpClient.post(this.END_POINT, entry).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToEntry)
+    )
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    const url = `${this.END_POINT}/${entry.id}`;
+
+    return this.httpClient.put(url, entry).pipe(
+      catchError(this.handleError),
+      map(() => entry)
+    )
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.END_POINT}/${id}`;
+
+    return this.httpClient.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    )
+  }
+
+
+  private jsonDataToEntries(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+    jsonData.forEach(element => entries.push(element as Entry));
+    return entries;
+  }
+
+  private jsonDataToEntry(jsonData: any): Entry {
+    return jsonData as Entry;
+  }
+
+  private handleError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO => ', error);
+    return throwError(error);
+  }
+}
