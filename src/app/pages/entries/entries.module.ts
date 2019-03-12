328a45e9bfde587498f67f970e18005fd2ffeea2
unknown <vicente.pspenha@gmail.com>
2019-03-11 21:49:33 -0300

    adicionando EntryListComponent no declarations

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EntriesRoutingModule
   ],
-  declarations: []
+  declarations: [
+    EntryListComponent
+  ]
 })
 export class EntriesModule { }
