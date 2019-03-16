5da12e5cbfa35323688f09446f0b844440de193e
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-16 14:24:00 -0300

    adicionando ptBR para datas

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -31,6 +31,20 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     redix: ','
   };
 
+  ptBR = {
+    firstDayOfWeek: 0,
+    dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+    dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sat"],
+    dayNamesMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa"],
+    monthNames: [
+      "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+      "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+    monthNamesShort: [ "Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul", "Ago", "Set", "Out", "Nov", "Dez" ],
+    today: 'Hoje',
+    clear: 'Limpar',
+    dateFormat: 'dd/mm/yy'
+};
+
   constructor(private entryService: EntryService,
               private route: ActivatedRoute,
               private router: Router,
