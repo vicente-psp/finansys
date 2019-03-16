3652a33eed96d4d1deaec36f8479b1b9d9e1d817
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-16 12:53:56 -0300

    adicionado campos (type, amount, date, paid e categoryId) no buildEntryForm

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -62,7 +62,12 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
     this.entryForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
-      description: [null]
+      description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
     });
   }
 
