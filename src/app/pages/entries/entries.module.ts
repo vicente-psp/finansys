78461b25f8b5fd029e39bf75a58e53a78ceb9113
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-16 12:47:57 -0300

    adicionando módulos ReactiveFormsModule, CalendarModule e IMaskModule

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -3,14 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { CalendarModule } from 'primeng/calendar';
+import { IMaskModule } from 'angular-imask';
 
 @NgModule({
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
   ],
   declarations: [
-    EntryListComponent
+    EntryListComponent,
+    EntryFormComponent
   ]
 })
 export class EntriesModule { }
