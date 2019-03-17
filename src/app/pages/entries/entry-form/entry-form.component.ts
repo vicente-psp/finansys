df00b8c4f1b6b5bf4c8b47c3be708775ee68fdac
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-17 17:22:43 -0300

    otimização nos métodos createEntry e updateEntry

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -135,8 +135,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private createEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
-
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
     this.entryService.create(entry).subscribe(
       entry => this.actionsForSuccess(entry),
       error => this.actionsForError(error)
@@ -144,8 +143,7 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
   }
 
   private updateEntry() {
-    const entry: Entry = Object.assign(new Entry(), this.entryForm.value);
-
+    const entry: Entry = Entry.fromJson(this.entryForm.value);
     this.entryService.update(entry).subscribe(
       entry => this.actionsForSuccess(entry),
       error => this.actionsForError(error)
