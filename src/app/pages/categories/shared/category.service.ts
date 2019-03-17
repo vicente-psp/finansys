42b8c3e1d000cc3ba91d34d9a723c6fadb5de31c
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-17 17:11:03 -0300

    removido import não utilizado

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Injector } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { Category } from './category.model';
 
