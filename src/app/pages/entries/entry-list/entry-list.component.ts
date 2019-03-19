277327e98e87c11344946dfa16cdd3fa24616610
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-19 20:26:25 -0300

    melhoria (reduzindo duplicidade de códigos) estendendo de BaseResourceListComponent

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,41 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component'
 
 import { Entry } from '../shared/entry.model';
 import { EntryService } from '../shared/entry.service';
-import { error } from 'util';
-import { element } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-entry-list',
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
-  entries: Entry[] = [];
-
-  constructor(private entryService: EntryService) { }
-
-  ngOnInit() {
-    this.listEntries();
-  }
-
-  listEntries(){
-    this.entryService.getAll().subscribe(
-      entries => this.entries = entries.sort((a,b) => b.id - a.id),
-      error => alert('Erro ao listar')
-    )
-  }
-
-  deleteEntry(entry: Entry){
-    const confirmDelete = confirm('Deseja remover o registro?');
-
-    if (confirmDelete) {
-      this.entryService.delete(entry.id).subscribe(
-        () => this.entries = this.entries.filter(element => element != entry),
-        () => alert('Erro ao remover registro')
-      )
-    }
+  constructor(private entryService: EntryService) {
+    super(entryService);
   }
 
 }
