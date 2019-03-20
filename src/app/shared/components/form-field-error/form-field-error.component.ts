4b4ffd1728188d5e36110c77398ddcb05c671ed4
unknown <vicente.pspenha@gmail.com>
2019-03-20 20:25:37 -0300

    criando componente

---
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field-error',
+  template: `
+    <p>
+      form-field-error works!
+    </p>
+  `,
+  styleUrls: ['./form-field-error.component.css']
+})
+export class FormFieldErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
