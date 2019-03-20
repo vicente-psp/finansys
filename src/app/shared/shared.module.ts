12907788b0e94792586e58cbaa4cd44510bf8a98
unknown <vicente.pspenha@gmail.com>
2019-03-20 19:35:46 -0300

    importação do componente page-header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
   imports: [
@@ -11,13 +12,15 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
     RouterModule
   ],
   declarations: [
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    PageHeaderComponent
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     BreadCrumbComponent,
-    RouterModule
+    RouterModule,
+    PageHeaderComponent
   ]
 })
 export class SharedModule { }
