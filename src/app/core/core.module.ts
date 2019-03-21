56d7d434ff927fa0ce75cd11bc2c00bd53bd03ea
unknown <vicente.pspenha@gmail.com>
2019-03-20 21:42:41 -0300

    incluído componente NavbarComponent e módulo RouterModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   imports: [
@@ -13,13 +15,17 @@ import { InMemoryDatabase } from '../in-memory-database';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
   ],
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
