0f75c2c40aa9e98a6b7b7c938b84a423520931d8
unknown <vicente.pspenha@gmail.com>
2019-03-20 21:19:41 -0300

    criação do componente ServerErrorMessagesComponent

---
+++ src/app/shared/components/server-error-messages/server-error-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-error-messages',
+  templateUrl: './server-error-messages.component.html',
+  styleUrls: ['./server-error-messages.component.css']
+})
+export class ServerErrorMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
