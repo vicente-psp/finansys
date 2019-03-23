ba93cdcf5defc86044861420e9b8289a24b3025a
Vicente Silva <vicente.pspenha@gmail.com>
2019-03-23 12:50:55 -0300

    correção em comparações do método filterByMonthAndYear

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -48,8 +48,8 @@ export class EntryService extends BaseResourceService<Entry> {
   private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
     return entries.filter(entry => {
       const entryDate = moment(entry.date, 'DD/MM/YYYY');
-      const monthMatches = entryDate.month() + 1 === month;
-      const yearMatches = entryDate.year() === year;
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
 
       if (monthMatches && yearMatches) return entry;
     })
